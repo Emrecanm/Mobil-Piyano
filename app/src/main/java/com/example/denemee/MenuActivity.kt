e6b432043b26ab1a40b572674ff85de8e4ec64cc
EMRECAN <emrecan.dzn.25@gmail.com>
2024-11-11 23:41:32 +0300

    mobil Piyano Commit

--- app/src/main/java/com/example/denemee/MenuActivity.kt
+++ app/src/main/java/com/example/denemee/MenuActivity.kt
@@ -2,29 +2,27 @@ package com.example.denemee
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?)
-    {
+class MenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_menu)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        val button = findViewById<ImageButton>(R.id.menuButton)
+        val button = findViewById<ImageButton>(R.id.menuButton2)
 
-        button.setOnClickListener{
-            val intent= Intent (this,MenuActivity::class.java)
+        button.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
     }

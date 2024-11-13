500b9c7c21ae5cea1ce1f5c403453da44d9e1f79
BugOR <145480350+mrBayCE@users.noreply.github.com>
2024-11-13 14:56:20 +0300

    Şarkı listesi eklendi v2

--- app/src/main/java/com/example/denemee/SarkiListesi.kt
+++ app/src/main/java/com/example/denemee/SarkiListesi.kt
@@ -13,7 +13,13 @@ class SarkiListesi : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_sarki_listesi)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
 
+
+        }
 
 
 

ef6648d5bb16c2b34f9ed2f3ada2ea2c14c9388b
BugOR <145480350+mrBayCE@users.noreply.github.com>
2024-11-13 15:00:49 +0300

    Şarkı listesi eklendi buton

--- app/src/main/java/com/example/denemee/SarkiListesi.kt
+++ app/src/main/java/com/example/denemee/SarkiListesi.kt
@@ -1,7 +1,9 @@
 package com.example.denemee
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.ViewGroup
+import android.widget.ImageButton
 import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -18,9 +20,12 @@ class SarkiListesi : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
 
-
         }
-
+        val button = findViewById<ImageButton>(R.id.back1Button3)
+        button.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
 
 
     }
